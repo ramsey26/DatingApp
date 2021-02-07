062dc1f4e1e63cd51c4150469f9608d22d193bf1
Vyas, Ayush <1994vyas@gmail.com>
2021-02-07 21:36:49 +0530

    End of Section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +25,8 @@ namespace API.Controllers
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
 
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username.ToLower());
+            var user = await _context.Users.Include(p=>p.Photos)
+                            .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username.ToLower());
             if (user == null) return Unauthorized("Invalid Username");
 
             using var hmac = new HMACSHA512(user.PasswordSalt);
@@ -36,9 +38,10 @@ namespace API.Controllers
             }
 
             return new UserDTO
-            {
+            { 
                 Username = user.UserName,
-                Token = _tokenService.CreateToken(user)
+                Token = _tokenService.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
             };
         }
 
